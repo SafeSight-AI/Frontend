42c822d6082b90c85b96076c0b9f8b95b918411d
xp-squared <108478666+xp-squared@users.noreply.github.com>
2025-05-29 19:22:14 -0400

    TEST TEST TEST

--- frontend/src/components/ActiveAlerts.jsx
+++ frontend/src/components/ActiveAlerts.jsx
@@ -1,10 +1,59 @@
 import "./ActiveAlerts.css";
+import check from "../assets/check.png";
+import exclamation from "../assets/exclamation.png";
+import { useState } from 'react';
+
+// these links both have actual good examples of how to use useState and useEffect
+// https://react.dev/reference/react/useEffect
+// https://react.dev/reference/react/useState
 
 const ActiveAlerts = () => {
-    return (
-        <div className='active-alerts-container'>
-        </div>
-    );
+    // State to manage alert status
+    const [alertActive, setAlert] = useState(false);
+    // Function to simulate an alert trigger
+    const triggerAlert = () => {
+        if (!alertActive) {
+            // Set alert to active state
+            setAlert(true);
+            // Simulate a violation alert for 1 second, set back to safe state after
+            setTimeout(() => {setAlert(false); }, 1000);
+        }
+};
+
+
+/* Production code, work in progress
+
+    // API URL to fetch active alerts, replace with actual endpoint and is constant because will never have to change
+    const apiUrl = !!!
+
+    const ActiveAlerts = () => {
+        const [alertActive, setAlert] = useState(false);
+        useEffect(() => {
+            // Call the API and parse the result
+            const response = await fetch(apiUrl);
+
+            ...
+            
+        });
+    
+    }
+
+*/
+
+
+  return (
+    <div className='active-alerts-container'>
+      <img
+        src={alertActive ? exclamation : check}
+        alt={alertActive ? "Danger" : "Safe"}
+        className='alert-icon'
+      />
+      {/* Test button to simulate violation */}
+      <button onClick={triggerAlert} className='trigger-button'>
+        Simulate Violation
+      </button>
+    </div>
+  );
 };
 
 export default ActiveAlerts;
